06820dc4b39e23d18c70af2acc749f9f112304ee
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-17 08:35:35 +0800

    Added Border component

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
     <Navigation />
     <b-container class="p-0" fluid>
       <About />
+      <Border />
       <Experience />
+      <Border />
       <Education />
+      <Border />
       <Skills />
+      <Border />
       <Interests />
+      <Border />
       <Awards />
     </b-container>
   </div>
 </template>
 
 <script>
-import smoothScroll from 'smooth-scroll'
+import smoothScroll from 'smooth-scroll';
 
 import About from "@/components/About";
 import Experience from "@/components/Experience";
@@ -22,6 +27,7 @@ import Skills from "@/components/Skills";
 import Interests from "@/components/Interests";
 import Awards from "@/components/Awards";
 import Navigation from "@/components/Navigation";
+import Border from "@/components/Border";
 
 export default {
   name: "app",
@@ -32,7 +38,8 @@ export default {
     Skills,
     Interests,
     Awards,
-    Navigation
+    Navigation,
+    Border
   },
   created: function() {
     var scroll = new smoothScroll('a[href*="#"]', {
