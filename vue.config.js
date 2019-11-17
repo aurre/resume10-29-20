35f7fced3ff1555ce750193c0cb16927c05dba41
webdevsuperfast <acob.rotsen@hotmail.com>
2019-11-17 10:48:37 +0800

    Set publicPath for gh-pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const GoogleFontsPlugin = require('google-fonts-plugin')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/startbootstrap-resume-vue' : '/',
   configureWebpack: {
     plugins: [
       new GoogleFontsPlugin('./src/fonts.json')
